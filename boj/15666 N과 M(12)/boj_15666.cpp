972f62a1d062f717857263c897f345a6699ef8bd
JunHyung Park <yoseb95@gmail.com>
2024-02-05 10:02:14 +0000

    new file:   "boj/15666 N\352\263\274 M(12)/a.out" 	new file:   "boj/15666 N\352\263\274 M(12)/boj_15666.cpp"

---
+++ boj/15666 N과 M(12)/boj_15666.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N,M;
+
+set<int> arr;
+
+vector<vector<int>> rtn;
+
+void input() {
+    cin >> N >> M;
+    for(int i = 0; i < N; ++i){
+        int tmp;
+        cin >> tmp;
+        arr.insert(tmp);
+    }
+}
+
+void dfs(int n) {
+    
+}
+
+int main() {
+    input();
+
+}

e0d76d4f73679f1b65f073e4d0317981c6c854e7
JunHyung Park <yoseb95@gmail.com>
2024-02-05 14:08:50 +0000

    deleted:    "boj/15666 N\352\263\274 M(12)/a.out" 	modified:   "boj/15666 N\352\263\274 M(12)/boj_15666.cpp"

--- boj/15666 N과 M(12)/boj_15666.cpp
+++ boj/15666 N과 M(12)/boj_15666.cpp
@@ -7,6 +7,7 @@ int N,M;
 set<int> arr;
 
 vector<vector<int>> rtn;
+vector<int> tmp_arr;
 
 void input() {
     cin >> N >> M;
@@ -17,11 +18,32 @@ void input() {
     }
 }
 
+void print() {
+    for(int i = 0; i < rtn.size(); ++i) {
+        for(int j = 0; j < rtn[i].size(); ++j)
+            cout << rtn[i][j] <<" ";
+        cout << "\n";
+    }
+}
+
 void dfs(int n) {
-    
+    if(n == 0) {
+        rtn.push_back(tmp_arr);
+        return;
+    }
+    for(auto it = arr.begin(); it != arr.end(); ++it) {
+        if(tmp_arr.empty() || *it >= tmp_arr.back()) {
+            tmp_arr.push_back(*it);
+            dfs(n - 1);
+            tmp_arr.pop_back();
+        }
+    }
 }
 
 int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
     input();
-
+    dfs(M);
+    print();
 }

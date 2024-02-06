12dad9ceca0254e1f857dc30a23f598da12a93c2
JunHyung Park <yoseb95@gmail.com>
2024-02-06 14:40:47 +0000

    modified:   "boj/1941 \354\206\214\353\254\270\353\202\234 \354\271\240\352\263\265\354\243\274/boj_1941.cpp"

--- boj/1941 소문난 칠공주/boj_1941.cpp
+++ boj/1941 소문난 칠공주/boj_1941.cpp
@@ -21,13 +21,41 @@ void print () {
             cout << check[i][j] << " ";
         cout << "\n";
     }
+    cout << "=================\n";
 }
 
-bool check_cross(int n) {
-    int x = n % 5;
-    int y = n / 5;
+bool check_cross(int x, int y) {
     if((x > 0 && check[y][x - 1] == true) || (y > 0 && check[y - 1][x] == true) || \
         (y < 4 && check[y + 1][x] == true) || (x < 4 && check[y][x+1] == true))
+        return true;
+    return false;
+}
+
+bool check_map(int x, int y) {
+    int cnt_visit = 0;
+    int cnt_s = 0;
+    bool visit[5][5] = {false};
+    int dir[4] = {1,0,-1,0};
+    queue<pair<int,int>> que;
+
+    que.push({x, y});
+    visit[y][x] = true;
+    while(!que.empty()) {
+        auto [x , y] = que.front();
+        ++cnt_visit;
+        if(Map[y][x] == 'S')
+            ++cnt_s;
+        for (int i = 0; i < 4; ++i) {
+            int next_x = x + dir[i];
+            int next_y = y + dir[(i + 1) % 4];
+            if(next_x < 0 || next_y < 0 || next_x >= 5 || next_y >=5 || check[next_y][next_x] == false || visit[next_y][next_x] == true)
+                continue;
+            visit[next_y][next_x] = true;
+            que.push({next_x, next_y});
+        }
+        que.pop();
+    }
+    if (cnt_visit == 7 && cnt_s >= 4)
         return true;
     return false;
 }
@@ -35,39 +63,25 @@ bool check_cross(int n) {
 void dfs(int n, int dep) {
     int x = n % 5;
     int y = n /5;
-    cout << "dep: " << dep << " n: " << n << " x: " << x << " y: " << y << "\n";
-    print();
-    if ((dep >= 1 && check[y][x]) || (dep > 1 && !check_cross(n))) {
-        cout << "cross fail\n";
-        return ;
-    }
     if(dep == 7) {
-        ++ans;
-        cout << " ans++\n";
+            // cout << "dep: " << dep << " n: " << n << " x: " << x << " y: " << y << "\n";
+        if(check_map(x, y)) {
+            // print();
+            ++ans;
+        }
         return ;
     }
-    if(dep >= 1)
-        check[y][x] = true;
-    for(int i = 0; i < 25; ++i)  {
-        if (Map[i / 5][i % 5] == 'Y') {
-            if(un_s == 3) {
-                cout << "un_s == 3 \n";
-                continue;
-            }
-            ++un_s;
-        }
-        dfs(i, dep + 1);
-        if (Map[i/5][i%5] == 'Y')
-            --un_s;
+    for(int i = n + 1; i < 25; ++i)  {
+        check[i /5][i % 5] = true;
+        dfs(i , dep + 1);
+        check[i /5][i % 5] = false;
     }
-    if(dep >= 1)
-        check[y][x] = false;
 }
 
 int main() {
     memset(check, false, sizeof check);
     input();
-    print();
+    // print();
     dfs(-1,0);
     cout << ans;
 }

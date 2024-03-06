4d3e9b409c4c142aa30df3f7e784ba2d730b5da0
JunHyung Park <yoseb95@gmail.com>
2024-03-06 11:51:57 +0000

    modified:   "boj/2457 \352\263\265\354\243\274\353\213\230\354\235\230 \354\240\225\354\233\220/boj_2457.cpp"

--- boj/2457 공주님의 정원/boj_2457.cpp
+++ boj/2457 공주님의 정원/boj_2457.cpp
@@ -13,27 +13,16 @@ bool checkFastMonth(int m1, int d1, int m2, int d2) {
 int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
     cin >> N;
-    vector<vector<int>> vec;
+    vector<vector<int>> vec(N,vector<int>(4));
     int ans = 1, m = 3, d = 1, em_tmp = 0, ed_tmp = 0;
-    for(int i = 0, sm, sd, em, ed; i < N; ++i) {
-        cin >> sm >> sd >> em >> ed;
-        if (checkFastMonth(sm, sd, em, ed)){
-            vec.push_back({sm,sd,em,ed});
-        } else {
-            vec.push_back({sm,sd,12,31});
-            vec.push_back({1,1,em,ed});
-        }
-    }
+    for(int i = 0; i < N; ++i)
+        cin >> vec[i][0] >> vec[i][1] >> vec[i][2] >> vec[i][3];
     sort(vec.begin(), vec.end());
-    // for(int i = 0; i < vec.size(); ++i)
-    //     cout << vec[i][0] << " " << vec[i][1] << " "<< vec[i][2] << " " << vec[i][3] << "\n";
     for(int i = 0; i < vec.size(); ++i) {
         if(checkFastMonth(m, d, vec[i][0],vec[i][1])) {
-
             ++ans;
             m = em_tmp;
             d = ed_tmp;
-            // cout << m << " " << d << " "<<vec[i][0] << " " <<vec[i][1] <<"\n";
         }
         if (checkFastMonth(em_tmp, ed_tmp,vec[i][2], vec[i][3])) {
             if(checkFastMonth(m ,d, vec[i][0], vec[i][1])) {

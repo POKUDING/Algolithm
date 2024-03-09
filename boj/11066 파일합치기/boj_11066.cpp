859d7194ef337ba2f98af936a5731cecd6387a28
JunHyung Park <yoseb95@gmail.com>
2024-03-09 14:59:53 +0000

    deleted:    "boj/11066 \355\214\214\354\235\274\355\225\251\354\271\230\352\270\260/a.out" 	modified:   "boj/11066 \355\214\214\354\235\274\355\225\251\354\271\230\352\270\260/boj_11066.cpp" 	deleted:    "boj/11066 \355\214\214\354\235\274\355\225\251\354\271\230\352\270\260/tmp"

--- boj/11066 파일합치기/boj_11066.cpp
+++ boj/11066 파일합치기/boj_11066.cpp
@@ -5,22 +5,24 @@ using namespace std;
 int T, K, tmp;
 
 int main() {
-    priority_queue<int> pq;
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
     cin >> T;
     while(T--) {
         cin >> K;
-        for(int i = 0; i < K; ++i) {
-            cin >> tmp;
-            pq.push(-tmp);
+        vector<vector<int>> dp(K + 1,vector<int>(K + 1));
+        vector<int> sum(K+1);
+        for(int i = 1; i <= K; ++i) {
+            cin >> sum[i];
+            sum[i] += sum[i - 1];
         }
-        while(pq.size() > 1) {
-            tmp = pq.top();
-            pq.pop();
-            tmp += pq.top();
-            pq.pop();
-            pq.push(tmp);
+        for(int i = 1; i <= K; ++i) {
+            for(int j = 1; j + i <= K; ++j) {
+                dp[j][j + i] = INT_MAX;
+                for(int mid = j; mid < j + i; ++mid) {
+                    dp[j][j + i] = min(dp[j][j + i], dp[j][mid] + dp[mid + 1][j + i] + sum[j + i] - sum[j - 1]);
+                }
+            }
         }
-        cout << -pq.top() << "\n";
-        pq.pop();
+        cout << dp[1][K] <<"\n"; 
     }
 }

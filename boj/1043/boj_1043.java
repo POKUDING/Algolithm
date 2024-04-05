6bf3575ee5c65873572c71d1b08d5fbdc8bdc90e
JunHyung Park <yoseb95@gmail.com>
2024-04-05 17:03:46 +0000

    new file:   boj/1043/boj_1043.java

---
+++ boj/1043/boj_1043.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main {
+    static BufferedReader br = new bufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void init() {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
